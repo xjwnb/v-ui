1d9b46e3b9cdc105d43cf1f4cce74fa4fafb9d34
1565163445@qq.com <1565163445@qq.com>
2020-12-07 23:39:49 +0800

    update button

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- 按钮 -->
+    <vp-button type="default">default</vp-button>
+    <vp-button type="primary">primary</vp-button>
+    <vp-button type="success">success</vp-button>
+    <vp-button type="info">info</vp-button>
+    <vp-button type="warning">warning</vp-button>
+    <vp-button type="danger">danger</vp-button>
+    <vp-button type="primary" round>round - primary按钮</vp-button>
+    <vp-button type="primary" @click="clickHandle">click-primary按钮</vp-button>
+    <vp-button type="primary" icon="iconfont icon-redupaixu">icon - primary按钮</vp-button>
+    <vp-button type="primary"><span class="iconfont icon-redupaixu"></span></vp-button>
   </div>
 </template>
 
 <script>
+import vpButton from './components/vp-button';
 
 export default {
   name: 'App',
   components: {
+    vpButton
+  },
+  methods: {
+    clickHandle(e) {
+      console.log(e);
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
+
 import Vue from 'vue'
 import App from './App.vue'
+
+import "./assets/iconfont/iconfont.css"
 
 Vue.config.productionTip = false
 
