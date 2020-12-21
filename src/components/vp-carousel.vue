2df9b32b0380951ad94c753d8dcee96e9d8f4105
1565163445@qq.com <1565163445@qq.com>
2020-12-22 02:26:46 +0800

    update carousel增加功能

--- src/components/vp-carousel.vue
+++ src/components/vp-carousel.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="vp-carousel" :style="{ height: height + 'px' }">
+    <div class="vp-carousel-arrow">
+      <ul class="vp-carousel-arrow-ul">
+        <li
+          class="vp-carousel-arrow-li"
+          v-for="(slot, index) in $slots.default"
+          :key="index"
+        >
+          <button :class="{'vp-carousel-arrow-button': true, 'vp-carousel-button-isActive': (currentIndex === index ? true : false)}"></button>
+        </li>
+      </ul>
+    </div>
     <div class="vp-carousel-container">
       <slot></slot>
     </div>
@@ -23,7 +34,7 @@ export default {
       type: Number,
       default: 3000,
     },
-/*     direction: {
+    /*     direction: {
       type: String,
       default: "horizontal",
     }, */
@@ -40,7 +51,8 @@ export default {
     return {
       timer: null,
       timeoutTimer: null,
-      intervalTime: null,
+      // intervalTime: null,
+      currentIndex: 0
     };
   },
   created() {},
@@ -64,7 +76,6 @@ export default {
     });
 
     // 移动内容
-    let currentIndex = 0;
     this.timer = setInterval(() => {
       for (let i = 0, l = slotsLength; i < l; i++) {
         let transform = slots[i].elm.style.transform;
@@ -72,12 +83,13 @@ export default {
         let transformNum = Number(transform.match(reg)[0]);
         slots[i].elm.style.transform = `translate(${transformNum - 100}%)`;
       }
-      if (currentIndex === slotsLength - 1) {
-        currentIndex = 0;
-      }
+      
       // console.log(currentIndex);
-      ++currentIndex;
-      console.log(currentIndex);
+      ++this.currentIndex;
+      if (this.currentIndex === slotsLength) {
+        this.currentIndex = 0;
+      }
+      console.log(this.currentIndex);
       for (let i = 0, l = slotsLength; i < l; i++) {
         slots[i].elm.style.opacity = 1;
         let transform = slots[i].elm.style.transform;
@@ -122,5 +134,52 @@ export default {
   position: relative;
   width: 100%;
   height: 100%;
+}
+.vp-carousel-arrow {
+  position: absolute;
+  display: flex;
+  height: auto;
+  z-index: 2;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+}
+.vp-carousel-arrow-ul {
+  list-style: none;
+  display: flex;
+  padding: 0;
+  margin: 0;
+  justify-content: center;
+  align-content: flex-end;
+}
+.vp-carousel-arrow-li {
+  display: flex;
+  align-items: center;
+  padding: 10px 0;
+}
+
+.vp-carousel-arrow-button {
+  margin: 0;
+  padding: 0;
+  outline: none;
+  border-radius: 0;
+  background-color: transparent;
+  line-height: inherit;
+  width: max-content;
+}
+.vp-carousel-arrow-button:after {
+  border: none;
+}
+.vp-carousel-arrow-button {
+  background-color: lightblue;
+  border: none;
+  width: 30px;
+  height: 2px;
+  border-radius: 2px;
+  margin: 3px 5px;
+  padding: 2px;
+}
+.vp-carousel-button-isActive {
+  background-color: aqua;
 }
 </style>
