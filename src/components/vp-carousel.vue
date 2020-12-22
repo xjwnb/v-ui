c6e2ce5350f128802319489eaa84d93e60d7fe4a
1565163445@qq.com <1565163445@qq.com>
2020-12-22 16:36:12 +0800

    update carousel

--- src/components/vp-carousel.vue
+++ src/components/vp-carousel.vue
@@ -7,7 +7,13 @@
           v-for="(slot, index) in $slots.default"
           :key="index"
         >
-          <button :class="{'vp-carousel-arrow-button': true, 'vp-carousel-button-isActive': (currentIndex === index ? true : false)}"></button>
+          <button
+            :class="{
+              'vp-carousel-arrow-button': true,
+              'vp-carousel-button-isActive':
+                currentIndex === index ? true : false,
+            }"
+          ></button>
         </li>
       </ul>
     </div>
@@ -52,7 +58,7 @@ export default {
       timer: null,
       timeoutTimer: null,
       // intervalTime: null,
-      currentIndex: 0
+      currentIndex: 0,
     };
   },
   created() {},
@@ -77,19 +83,165 @@ export default {
 
     // 移动内容
     this.timer = setInterval(() => {
+      this.intervalChangeFunc(slots, slotsLength);
+      /*       for (let i = 0, l = slotsLength; i < l; i++) {
+        let transform = slots[i].elm.style.transform;
+        let reg = /(-)?[0-9]+/g;
+        let transformNum = Number(transform.match(reg)[0]);
+        if (transformNum === 0) {
+          this.currentIndex = i + 1;
+          if (this.currentIndex === slotsLength) {
+            this.currentIndex = 0;
+          }
+        }
+        slots[i].elm.style.transform = `translate(${transformNum - 100}%)`;
+      }
+
+      // console.log(currentIndex);
+      // ++this.currentIndex;
+      // if (this.currentIndex === slotsLength) {
+      //   this.currentIndex = 0;
+      // }
+      for (let i = 0, l = slotsLength; i < l; i++) {
+        slots[i].elm.style.opacity = 1;
+        let transform = slots[i].elm.style.transform;
+        let reg = /(-)?[0-9]+/g;
+        let transformNum = Number(transform.match(reg)[0]);
+        let lastSlot = slots[slotsLength - 1].elm.style.transform;
+        let regExp = /[0-9]+/g;
+        let transformLastSlotNum = Number(lastSlot.match(regExp)[0]);
+        if (transformNum <= -200) {
+          let transformArr = [];
+          for (let i = 0, l = slotsLength; i < l; i++) {
+            let transform = slots[i].elm.style.transform;
+            let reg = /(-)?[0-9]+/g;
+            transformArr.push(Number(transform.match(reg)[0]));
+          }
+          // 降序
+          let newTransformArr = transformArr.sort(function (a, b) {
+            return b - a;
+          });
+          slots[i].elm.style.opacity = 0;
+          slots[i].elm.style.transform = `translate(${
+            newTransformArr[0] + 100
+          }%)`;
+        }
+      } */
+    }, this.interval);
+
+    let btns = this.$el.getElementsByClassName("vp-carousel-arrow-button");
+    console.log(btns);
+    let _this = this;
+    btns.forEach((btn, index) => {
+      console.log(btn);
+      btn.addEventListener("mouseover", function () {
+        _this.currentIndex = index;
+        clearInterval(_this.timer);
+        _this.changefirst(index);
+        for (let i = 0, l = btns.length; i < l; i++) {
+          btns[i].classList.remove("vp-carousel-button-isActive");
+        }
+        this.classList.add("vp-carousel-button-isActive");
+      });
+      btn.addEventListener("mouseleave", function () {
+        _this.timer = setInterval(() => {
+          _this.intervalChangeFunc(slots, slotsLength);
+        }, _this.interval);
+      });
+    });
+  },
+  beforeDestroy() {
+    clearInterval(this.timer);
+    clearTimeout(this.timeoutTimer);
+  },
+  methods: {
+    changefirst(index) {
+      let slots = this.$slots.default;
+      let slotsLength = slots.length;
+      let oldCurrentIndex;
       for (let i = 0, l = slotsLength; i < l; i++) {
         let transform = slots[i].elm.style.transform;
         let reg = /(-)?[0-9]+/g;
         let transformNum = Number(transform.match(reg)[0]);
+        if (transformNum === 0) {
+          oldCurrentIndex = i;
+          break;
+        }
+      }
+      for (let ii = 0; ii < slotsLength; ii++) {
+        if (ii === index) {
+          slots[ii].elm.style.transform = `translate(0%)`;
+        } else if (ii > index) {
+          slots[ii].elm.style.transform = `translate(${(ii - index) * 100}%)`;
+        } else {
+          slots[ii].elm.style.transform = `translate(-${(index - ii) * 100}%)`;
+          let transform = slots[ii].elm.style.transform;
+          let reg = /(-)?[0-9]+/g;
+          let transformNum = Number(transform.match(reg)[0]);
+          console.log(transformNum);
+          /* if (transformNum <= -200) {
+            let transformArr = [];
+            for (let i = 0, l = slotsLength; i < l; i++) {
+              let trans = slots[i].elm.style.tranWsform;
+              let reg = /(-)?[0-9]+/g;
+              transformArr.push(Number(trans.match(reg)[0]));
+            }
+            // 降序
+            let newTransformArr = transformArr.sort(function (a, b) {
+              return b - a;
+            });
+            slots[ii].elm.style.opacity = 0;
+            slots[ii].elm.style.transform = `translate(${
+              newTransformArr[0] + 100
+            }%)`;
+          } */
+          for (let i = 0, l = slotsLength; i < l; i++) {
+            slots[i].elm.style.opacity = 1;
+            let transform = slots[i].elm.style.transform;
+            let reg = /(-)?[0-9]+/g;
+            let transformNum = Number(transform.match(reg)[0]);
+            let lastSlot = slots[slotsLength - 1].elm.style.transform;
+            let regExp = /[0-9]+/g;
+            let transformLastSlotNum = Number(lastSlot.match(regExp)[0]);
+            if (transformNum <= -200) {
+              let transformArr = [];
+              for (let i = 0, l = slotsLength; i < l; i++) {
+                let transform = slots[i].elm.style.transform;
+                let reg = /(-)?[0-9]+/g;
+                transformArr.push(Number(transform.match(reg)[0]));
+              }
+              // 降序
+              let newTransformArr = transformArr.sort(function (a, b) {
+                return b - a;
+              });
+              slots[i].elm.style.opacity = 0;
+              slots[i].elm.style.transform = `translate(${
+                newTransformArr[0] + 100
+              }%)`;
+            }
+          }
+        }
+      }
+    },
+    intervalChangeFunc(slots, slotsLength) {
+      for (let i = 0, l = slotsLength; i < l; i++) {
+        let transform = slots[i].elm.style.transform;
+        let reg = /(-)?[0-9]+/g;
+        let transformNum = Number(transform.match(reg)[0]);
+        if (transformNum === 0) {
+          this.currentIndex = i + 1;
+          if (this.currentIndex === slotsLength) {
+            this.currentIndex = 0;
+          }
+        }
         slots[i].elm.style.transform = `translate(${transformNum - 100}%)`;
       }
-      
+
       // console.log(currentIndex);
-      ++this.currentIndex;
-      if (this.currentIndex === slotsLength) {
+      // ++this.currentIndex;
+      /* if (this.currentIndex === slotsLength) {
         this.currentIndex = 0;
-      }
-      console.log(this.currentIndex);
+      } */
       for (let i = 0, l = slotsLength; i < l; i++) {
         slots[i].elm.style.opacity = 1;
         let transform = slots[i].elm.style.transform;
@@ -115,13 +267,8 @@ export default {
           }%)`;
         }
       }
-    }, this.interval);
+    },
   },
-  beforeDestroy() {
-    clearInterval(this.timer);
-    clearTimeout(this.timeoutTimer);
-  },
-  methods: {},
 };
 </script>
 <style scoped>
